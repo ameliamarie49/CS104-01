6a04fff7b49b924ece6b94d1d45637fbd483696b
ameliamarie49 <55414253+ameliamarie49@users.noreply.github.com>
2019-10-30 17:42:32 -0400

    Add files via upload

---
+++ richter scale.py
@@ -0,0 +1,18 @@
+value == true
+while (value == true):
+    richter = float (input("please enter a richter value")
+    if (richter = -99):
+        value= False
+        break
+    elif (richter > -99 and richter < 0):
+        print("enter a positive value")
+     elif (richter >= 8.0):
+        print ("most structures will fall")
+    elif(richter >=7.0):
+        print("many buildings were destroyed")
+    elif (richter >=6.0):
+        print ("many buildings considerably damaged, some collapse")
+    elif (richter >=4.5):
+        print ("damage to poorly constructed buildings")
+    else:
+        print ("no destreuction of buildings:")
